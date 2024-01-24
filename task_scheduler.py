53de6fbf0d4677b119d23f8938debb14facef4d2
Cassianvale <1544257291@qq.com>
2024-01-24 15:09:31 +0800

    first commit

---
+++ task_scheduler.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import configparser
+import shutil
+import subprocess
+import time
+import datetime
+import os
+
+# 创建一个配置文件解析器
+config = configparser.ConfigParser()
+
+# 读取配置文件
+read_data = config.read('config.ini', encoding='utf-8')
+task_type = config.get('Settings', 'task_type')
+backup_enabled = config.getboolean('Settings', 'backup_enabled')
+program_path = config.get('Settings', 'program_path')
+backup_source = config.get('Settings', 'backup_source')
+backup_target = config.get('Settings', 'backup_target')
+rcon_path = config.get('Settings', 'rcon_path')
+restart_interval_hours = config.get('Settings', 'restart_interval_hours')
+backup_interval_hours = config.get('Settings', 'backup_interval_hours')
+
+restart_interval = int(restart_interval_hours) * 3600
+datetime_now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+appName = 'PalServer-Win64-Test-Cmd.exe'
+
+
+# 备份任务
+def backup_task():
+
+    # 如果备份间隔不为空，则执行备份
+    if backup_interval_hours:
+        backup_interval = int(backup_interval_hours) * 3600  # 将备份间隔转换为秒
+
+        while True:
+            # 备份文件
+            print("自动备份已开启")
+            os.chdir(rcon_path)
+            rcon_process = subprocess.Popen(['rcon.exe'], stdin=subprocess.PIPE)
+            rcon_process.communicate(
+                input='Save'.encode())
+            print("已发送RCON存档指令，正在进行备份......")
+            time.sleep(2)
+            shutil.copytree(backup_source, os.path.join(backup_target, f"Saved_{datetime_now}"))
+            print("备份成功，文件名为：Saved_" + datetime_now)
+
+            # 显示倒计时并等待指定的备份间隔
+            for i in range(backup_interval, 0, -1):
+                print(f'\r下一次备份将在 {i} 秒后开始...', end='')
+                time.sleep(1)
+    else:
+        print("自动备份未开启，需要自动备份请需改config.ini配置")
+
+
+# 轮询任务(固定延迟执行)
+def polling_task():
+    """
+    基本逻辑
+    关闭运行的palserver服务 → 启动服务 → 等待服务重启→
+    倒计时还剩10秒打开rcon.exe发送 Broadcastt 服务器关服倒计时10s →
+    等待10秒 → 关闭rcon.exe → ......
+    """
+    while True:
+        # 结束当前正在运行的程序，忽略错误信息
+        print("正在关闭任何在运行的palserver服务......")
+        task_exists = subprocess.run(['tasklist', '/NH', '/FI', f'IMAGENAME eq {appName}*'], stdout=subprocess.PIPE,
+                                     text=True)
+        if appName in task_exists.stdout:
+            # 结束当前正在运行的程序，忽略错误信息
+            subprocess.run(['taskkill', '/f', '/im', appName], stderr=subprocess.DEVNULL)
+
+        # 启动程序
+        print("正在启动程序......")
+        subprocess.Popen([program_path])
+
+        # 服务器持续运行时间(重启间隔)
+        for i in range(restart_interval, 10, -1):
+            print(f'\r服务器将在 {i} 秒后重启......', end='')
+            time.sleep(1)
+
+        # 还剩10秒的时候自动进入rcon-0.10.3-win64目录
+        os.chdir(rcon_path)
+
+        # 在rcon-0.10.3-win64目录中启动rcon.exe并发送命令
+        rcon_process = subprocess.Popen(['rcon.exe'], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        # 显示倒计时并等待剩余的10秒
+        for i in range(10, 0, -1):
+            rcon_process.stdin.write(f'Broadcast The_server_is_about_to_restart_with_a_countdown_of_{i}_seconds.\n'.encode())
+            rcon_process.stdin.flush()  # 确保消息被发送
+            time.sleep(1)
+
+# 计划任务(每天特定时间执行)
+def scheduled_task():
+
+    # 结束当前正在运行的程序，忽略错误信息
+    print("正在关闭任何在运行的palserver服务......")
+    task_exists = subprocess.run(['tasklist', '/NH', '/FI', f'IMAGENAME eq {appName}*'], stdout=subprocess.PIPE,
+                                 text=True)
+    if appName in task_exists.stdout:
+        # 结束当前正在运行的程序，忽略错误信息
+        subprocess.run(['taskkill', '/f', '/im', appName], stderr=subprocess.DEVNULL)
+
+    # 启动程序
+    print("正在启动程序......")
+    subprocess.Popen([program_path])
+
+    # 对于每个计划任务，如果设置的时间不为空，则创建计划任务
+    for i in range(1, 4):
+        task_name = f'PalServerRestart{i}'
+        start_time = config.get('Settings', f'start_time_{i}')
+        if start_time:
+            # 删除已有的任务
+            subprocess.run(['schtasks', '/Delete', '/TN', task_name, '/F'], stderr=subprocess.DEVNULL)
+            print("当天计划任务已清空")
+            # 创建新任务
+            subprocess.run(['schtasks', '/Create', '/SC', 'DAILY', '/TN', task_name, '/TR',
+                            os.path.realpath(__file__), '/ST', start_time])
+            print(f"新计划任务'{task_name}'创建成功，每天将在{start_time}重启服务器")
+
+
+# 轮询任务
+if task_type == '1':
+    print("当前进行的是轮询任务，已读取config配置")
+    polling_task()
+# 计划任务
+elif task_type == '2':
+    print("当前进行的是计划任务，已读取config配置")
+    scheduled_task()
+else:
+    print(
+        f"配置 config.ini 中的任务类型配置项 '{task_type}'无效")
