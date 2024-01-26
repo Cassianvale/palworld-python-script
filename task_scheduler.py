3cbd9819cb19cac79c30c531cd5659eeea2a5d30
Cassianvale <1544257291@qq.com>
2024-01-26 14:31:32 +0800

    🎇fix: 修复一些严重bug

--- task_scheduler.py
+++ task_scheduler.py
@@ -5,13 +5,12 @@
 import subprocess
 import time
 import psutil
-import multiprocessing
 import read_conf
+import threading
 from utils.log_control import INFO
 from rcon.source import Client
 from rcon.source.proto import Packet
-from multiprocessing import Process
-import threading
+
 
 class TaskScheduler:
     def __init__(self):
@@ -21,7 +20,7 @@ class TaskScheduler:
         self.port = self.conf['rcon_port'],
         self.passwd = self.conf['rcon_password'],
         self.rcon_command = self.conf['rcon_command']
-        self.daemon_time  = self.conf['daemon_time']
+        self.daemon_time = self.conf['daemon_time']
         self.arguments = self.conf.get('arguments', '').split()
 
     # 修改rcon源代码，忽略SessionTimeout异常
@@ -38,7 +37,7 @@ class TaskScheduler:
 
     # Apply the monkey patch
     Client.run = patched_run
-    
+
     def start_program(self):
         INFO.logger.info("[ 启动任务 ] 正在启动程序......")
         print("[ 启动任务 ] 正在启动程序......")
@@ -53,10 +52,10 @@ class TaskScheduler:
             program_args.extend(["-useperfthreads", "-NoAsyncLoadingThread", "-UseMultithreadForDS"])
         print("[ 启动任务 ] 启动参数：", self.conf['arguments'].split())
         subprocess.Popen(program_args)
-            
+
     # 轮询任务(固定延迟执行)
     def polling_task(self):
-        
+
         while True:
             if self.conf['restart_interval'] < 60:
                 INFO.logger.error("[ 轮询任务 ] 服务器重启时间 restart_interval 必须大于等于1分钟，请重新设置！")
@@ -71,11 +70,11 @@ class TaskScheduler:
 
             INFO.logger.info(f'[ 轮询任务 ] 服务器将进入重启倒计时，设置时长为 {self.conf["restart_interval"]} 秒......')
             print(f'[ 轮询任务 ] 服务器将进入重启倒计时，设置时长为 {self.conf["restart_interval"]} 秒......')
-            
+
             # 如果设置了检查内存使用情况memory_monitor_enabled
             if self.conf['memory_monitor_enabled']:
                 print("[ 内存监控 ] 已开启内存监控，将在内存使用超过阈值时重启程序......")
-                
+
             # 服务器持续运行时间(重启间隔)
             for i in range(int(self.conf['restart_interval']), 0, -1):
                 if self.conf['memory_monitor_enabled']:
@@ -92,7 +91,7 @@ class TaskScheduler:
                         print("[ 内存监控 ] 轮询间隔 polling_interval_seconds 必须大于5秒，请重新设置！")
                         time.sleep(3)
                         exit(0)
-                
+
                 print(f'\r[ 轮询任务 ] 服务器将在 {i} 秒后重启......', end='')
 
                 # 还剩30秒的时候发送rcon关服消息提醒
@@ -110,7 +109,7 @@ class TaskScheduler:
                             response = client.run(f"{self.conf['rcon_command']} {message}", 'utf-8')
                             INFO.logger.info('Response:{0}'.format(response))
                             print('Response:', response)
-                            
+
                 print(f'\r[ 轮询任务 ] 服务器将在 {i} 秒后重启......', end='')
                 time.sleep(1)
             # 关闭服务端 放在循环的结尾,可以让用户不用关闭服务器的情况下启动本脚本
@@ -119,20 +118,20 @@ class TaskScheduler:
             print("[ 轮询任务 ] 正在关闭任何在运行的 PalServer 服务......")
             subprocess.run(['taskkill', '/f', '/im', self.appName], stderr=subprocess.DEVNULL)
 
-
     def start_daemon(self):
         # 守护进程代码
         while True:
             try:
                 # 检查服务是否在运行
-                result = subprocess.run(['tasklist', '/FI', 'IMAGENAME eq PalServer.exe'], capture_output=True, text=True)
+                result = subprocess.run(['tasklist', '/FI', 'IMAGENAME eq PalServer.exe'], capture_output=True,
+                                        text=True)
                 if 'PalServer.exe' not in result.stdout:
                     print("\r\033[K", end='')
-                    print('[ 守护进程 ] 监控到 PalServer 已停止,正在启动!!!')
+                    print('[ 守护进程 ] 监控到 PalServer 已停止，正在重新启动......')
 
                     # 启动程序
                     self.start_program()
-                        
+
                 else:
                     print("\r\033[K", end='')
                     print('[ 守护进程 ] 监控到 PalServer 正在运行中,无需任何操作')
@@ -146,7 +145,8 @@ class TaskScheduler:
                 print("\r\033[K", end='')
                 print(f"[ 守护进程 ] 程序异常终止，错误信息：{e}\n正在尝试重启程序......")
                 continue
-            
+
+
 def main():
     Task = TaskScheduler()
 
@@ -169,6 +169,7 @@ def main():
     polling_thread.join()
     if Task.conf['daemon_enabled']:
         daemon_thread.join()
-        
+
+
 if __name__ == '__main__':
     main()
