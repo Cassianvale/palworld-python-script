413626dbbd0416002df97f2aec12f3cb5bb069a9
MuCqq <1544257291@qq.com>
2024-01-29 01:57:49 +0800

    perf: 优化打包工具

--- pyinstaller.py
+++ pyinstaller.py
@@ -6,15 +6,17 @@ import shutil
 import os
 
 
+def remove_ds_store(dir):
+    # 删除DS_Store
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file == '.DS_Store':
+                os.remove(os.path.join(root, file))
+
+
 def package_scripts():
 
     scripts = ['task_scheduler.py', 'backup.py']
-
-    pyinstaller_path = shutil.which('pyinstaller')
-
-    if pyinstaller_path is None:
-        print('pyinstaller not found')
-        return
 
     for script in scripts:
         print('打包', scripts)
@@ -23,19 +25,20 @@ def package_scripts():
 
     shutil.copy('config.ini', 'dist')
 
+    if os.path.exists('dist/palworld-python-script.zip'):
+        os.remove('dist/palworld-python-script.zip')
+
     if os.path.exists('palworld-python-script.zip'):
         os.remove('palworld-python-script.zip')
 
     shutil.make_archive('palworld-python-script', 'zip', 'dist')
     print("palworld-python-script 打包成功！")
 
-    # 打包成功后放入dist文件夹，如果已存在则覆盖
-    if os.path.exists('dist/palworld-python-script.zip'):
-        os.remove('dist/palworld-python-script.zip')
     shutil.move('palworld-python-script.zip', 'dist')
     print("打包后的palworld-python-script.zip已放入dist文件夹！")
 
 
 if __name__ == '__main__':
+    remove_ds_store('dist')
     package_scripts()
 
